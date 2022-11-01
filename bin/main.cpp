400262f8b3f11ead588e48b540c0537d7aea31be
CHEYSOFF <slobberyblanket@gmail.com>
2022-11-01 19:40:17 +0300

    size writing in file works

--- bin/main.cpp
+++ bin/main.cpp
@@ -3,10 +3,21 @@
 #include "../lib/read.cpp"
 #include "../lib/write.cpp"
 
+//  file struct:
+//  BEFORE HAMMING
+//  name    bytes   bits
+//  size    5       40     
+//  name    21      168
+//  AFTER HAMMING
+//  name    bytes   bits
+//  size    8       64      
+//  name    32      256 
+
 int main() {
-    bool *hamming_char_bin = FromCharToHammingBinary(72);\
+    // bool *hamming_char_bin = FromCharToHammingBinary(72);
     
-    for (int i = 0; i < 12; i++) {
-        std::cout << hamming_char_bin[i];
-    }
+    // for (int i = 0; i < 12; i++) {
+    //     std::cout << hamming_char_bin[i];
+    // }
+    WriteSizeToFile("asba.txt", 1);
 }
